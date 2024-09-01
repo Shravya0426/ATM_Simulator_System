f1c6ff9cc7a6df0d1b9d7d2690a04e0c44cbd218
Shravya0426 <153540442+Shravya0426@users.noreply.github.com>
2024-09-02 00:00:12 +0530

    Update Conn.java

--- Conn.java
+++ Conn.java
@@ -2,14 +2,14 @@ package bank.management.system;
 
 import java.sql.*;
 
-//for connectiong..by using jdbc connectivity
+//for connecting..by using jdbc connectivity
 public class Conn {
     Statement s;
     Connection c;
     public Conn(){
         try{//mysql is an external entity...so to handle the runtime errors
               
-            c=DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","Shravya_26"); //creating connection
+            c=DriverManager.getConnection("jdbc:mysql:/bankmanagementsystem","root","Shravya_26"); //creating connection
             s=c.createStatement();  //creating the statement
             
         }catch(Exception e){
